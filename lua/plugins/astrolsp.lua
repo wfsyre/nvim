cdba0f2507c0572548e562a76b6f57400440daa1
William Syre <william.syre@gtri.gatech.edu>
2024-11-19 14:25:35 -0500

    changed some stuff

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -39,12 +39,12 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+      "pyright",
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
     -- customize how language servers are attached
     handlers = {
